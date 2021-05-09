c6598e55648b1605d3fbb0c35bdd4ef86239af08
Sergio Martin Rubio <econsergio@gmail.com>
2021-05-09 16:23:01 +0200

    Add files

---
+++ accounts.go
@@ -0,0 +1,54 @@
+package account
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Account struct {
+	Country      string `json:"country"`
+	BaseCurrency string `json:"base_currency"`
+	BankId       string `json:"bank_id"`
+}
+
+func create(account Account) (string, int) {
+	postBody, _ := json.Marshal(account)
+	response, err := http.Post("http://localhost:8080/v1/organisation/accounts/ad27e265-9605-4b4b-a0e5-3003ea9cc4dc", "application/json", bytes.NewBuffer(postBody))
+	if err != nil {
+		return err.Error(), http.StatusBadRequest
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body), http.StatusCreated
+}
+
+func fetch(accountId string) (string, int) {
+	response, err := http.Get("http://localhost:8080/v1/organisation/accounts/" + accountId)
+	if err != nil {
+		return err.Error(), http.StatusBadRequest
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body), http.StatusOK
+}
+
+func delete() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smartinrub/accounts
+
+go 1.16
